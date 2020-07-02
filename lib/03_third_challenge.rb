66070775ec30353cd866593cad9bbfdbdba778c7
Natalia Galan <n8galan@gmail.com>
2020-07-02 16:27:31 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,18 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => [
-        {
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
-        },
-        {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-        }
-      ]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {
@@ -41,18 +30,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => [
-        {
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
-        },
-        {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
-        }
-      ]
+      :heroine_friends => []
    }
   }
 
